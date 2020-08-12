68feab6e05c17d2326e8447ef53558b7bede32e0
Marc <mcavigli@gmail.com>
2020-08-11 22:54:42 -0700

    Added XML documentation for variables and methods and cleaned up code

--- Assets/Scripts/InfoBoxes.cs
+++ Assets/Scripts/InfoBoxes.cs
@@ -2,28 +2,90 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script contains each room's info boxes as well as the buttons used
+/// to navigate between rooms.
+/// </summary>
 public class InfoBoxes : MonoBehaviour
 {
-	// Living Room
-	public GameObject white;
+	// --- Living Room ---
+
+	/// <summary>
+	/// The panel that text is displayed on.
+	/// </summary>
 	public GameObject whitePanel;
+
+	/// <summary>
+	/// A button that gives information on the security guard, Bob.
+	/// </summary>
 	public GameObject bobButton;
+
+	/// <summary>
+	/// The information text that appears when the Bob button is selected.
+	/// </summary>
 	public GameObject bobPanel;
+
+	/// <summary>
+	/// The button that gives information about the library. 
+	/// </summary>
 	public GameObject libButton;
+
+	/// <summary>
+	/// The information text that appears when the library button is selected.
+	/// </summary>
 	public GameObject libPanel;
-	// Cube
+
+	// --- Cube ---
+
+	/// <summary>
+	/// The button that gives information about the cube.
+	/// </summary>
 	public GameObject cubeButton;
+
+	/// <summary>
+	/// The information text that appears when the cube button is selected.
+	/// </summary>
 	public GameObject cubePanel;
-	// Cantina
+
+	// --- Cantina ---
+
+	/// <summary>
+	/// The button that gives information about the cantina.
+	/// </summary>
 	public GameObject cantinaButton;
+
+	/// <summary>
+	/// The information text that appears when the cantina button is selected.
+	/// </summary>
 	public GameObject cantinaPanel;
-	// Mezzanine
+
+	// --- Mezzanine ---
+
+	/// <summary>
+	/// The button that gives information about the iMac matrix.
+	/// </summary>
 	public GameObject imacButton;
+
+	/// <summary>
+	/// The information text that appears when the iMac button is selected.
+	/// </summary>
 	public GameObject imacPanel;
+
+	/// <summary>
+	/// The button that gives information about the booths.
+	/// </summary>
 	public GameObject boothButton;
+
+	/// <summary>
+	/// The information text that appears when the booth button is selected.
+	/// </summary>
 	public GameObject boothPanel;
 
-	// Living Room
+	// --- Living Room ---
+
+	/// <summary>
+	/// Activates and deactivates the panel for the whiteboards.
+	/// </summary>
 	public void WhiteboardPanel()
 	{
 		if (whitePanel.activeSelf)
@@ -31,6 +93,10 @@ public class InfoBoxes : MonoBehaviour
 		else
 			whitePanel.SetActive(true);
 	}
+
+	/// <summary>
+	/// Activbates and deactivates the panel for Bob.
+	/// </summary>
 	public void BobPanel()
 	{
 		if (bobPanel.activeSelf)
@@ -38,6 +104,10 @@ public class InfoBoxes : MonoBehaviour
 		else
 			bobPanel.SetActive(true);
 	}
+
+	/// <summary>
+	/// Activates and deactivates the panel for the library.
+	/// </summary>
 	public void LibraryPanel()
 	{
 		if (libPanel.activeSelf)
@@ -46,7 +116,11 @@ public class InfoBoxes : MonoBehaviour
 			libPanel.SetActive(true);
 	}
 
-	// Cube
+	// --- Cube ---
+
+	/// <summary>
+	/// Activates and deactivates the panel for the cube.
+	/// </summary>
 	public void CubePanel()
 	{
 		if (cubePanel.activeSelf)
@@ -55,7 +129,11 @@ public class InfoBoxes : MonoBehaviour
 			cubePanel.SetActive(true);
 	}
 
-	// Cantina
+	// --- Cantina ---
+
+	/// <summary>
+	/// Activates and deactivates the panel for the cantina.
+	/// </summary>
 	public void CantinaPanel()
 	{
 		if (cantinaPanel.activeSelf)
@@ -64,7 +142,11 @@ public class InfoBoxes : MonoBehaviour
 			cantinaPanel.SetActive(true);
 	}
 
-	//Mezzanine
+	// --- Mezzanine ---
+
+	/// <summary>
+	/// Activates and deactivates the panel for the iMac matrix.
+	/// </summary>
 	public void ImacPanel()
 	{
 		if (imacPanel.activeSelf)
@@ -72,6 +154,10 @@ public class InfoBoxes : MonoBehaviour
 		else
 			imacPanel.SetActive(true);
 	}
+
+	/// <summary>
+	/// Activates and deactivates the panel for the booths.
+	/// </summary>
 	public void BoothPanel()
 	{
 		if (boothPanel.activeSelf)
